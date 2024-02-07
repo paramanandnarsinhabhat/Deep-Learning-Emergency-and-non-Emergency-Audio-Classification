f3805a6db9ff750cbe9c2dfc9a9d791b35cc066d
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-02-07 11:09:06 +0530

    updates

--- source/emergency_nonEmergency_audioclassification.py
+++ source/emergency_nonEmergency_audioclassification.py
@@ -5,3 +5,32 @@
 
 # Understanding the Problem Statement
 
+'''
+According to the National Crime Records Bureau, nearly 24,012 people die each day due to a delay in getting medical assistance. Many accident victims wait for help at the site, and a delay costs them their lives. The reasons could range from ambulances stuck in traffic to the fire brigade not being able to reach the site on time due to traffic jams. 
+
+The solution to the above problem is to create a system that automatically detects the emergency vehicle prior to reaching the traffic signals and change the traffic signals accordingly.
+
+
+'''
+
+# Dataset
+'''
+Download the dataset from [here](https://drive.google.com/file/d/1VBI_X6GyYvf8j3T70-_hVDyhR_sUzeCr/view?usp=sharing)
+
+<br>
+
+## Import Libraries
+
+Let us first import the libraries into our environment
+
+* **Librosa** is an open source library in Python that is used for audio and music analyis
+
+* **Scipy** is a python library for scientific & technical computing. It contains modules for signal processing, image processing, and linear algebera
+
+
+'''
+
+# For audio processing
+import librosa
+import scipy
+
